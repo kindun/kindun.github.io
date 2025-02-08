bf167d2c9f1476596a5c82f22cbfa7ca52e6d716
kindun <suzudai2118@gmail.com>
2025-02-08 17:54:33 +0900

    Add files via upload

--- script1.js
+++ script1.js
@@ -24,3 +24,44 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+
+function showInput(NUM) {
+  const output = document.getElementById("output" + NUM);
+  const value = document.getElementById("input" + NUM).value;
+  output.textContent = value;
+  document.getElementById("input" + NUM).style.display = "none";
+  document.getElementById("button" + NUM).style.display = "none";
+  document.getElementById("delete" + NUM).style.display = "inline";
+}
+
+function deleteInput(NUM) {
+  const input = document.getElementById("input" + NUM);
+  input.style.display = "inline";
+  input.value = "";
+  document.getElementById("button" + NUM).style.display = "inline";
+  document.getElementById("delete" + NUM).style.display = "none";
+  document.getElementById("output" + NUM).textContent = "";
+}
+
+let count = 4;
+
+function add() {
+  const tags = document.querySelector(".tag");
+  const newTag = tags.cloneNode(true);
+  
+  count++; // カウンタをインクリメント
+  const newIdBase = count; // 新しいIDの番号を取得
+  newTag.id = newIdBase;
+  console.log(newTag.id);
+  // 複製したdiv内のIDを変更
+  const elements = newTag.querySelectorAll("[id]");
+  elements.forEach(element => {
+    const oldId = element.id;
+    console.log(oldId);
+    const newId = oldId.replace(/\d+/, newIdBase); // 数字部分を新しいIDに置き換え
+    console.log(newId);
+    element.id = newId; // IDを変更
+  });
+
+  document.getElementById("input").appendChild(newTag);
+}
